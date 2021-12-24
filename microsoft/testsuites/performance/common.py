1a7306d5a62c101851199af3542ad015b952d61a
Lili Deng <lildeng@microsoft.com>
2021-12-24 18:23:32 +0800

    Add dict_to_fields method.

--- microsoft/testsuites/performance/common.py
+++ microsoft/testsuites/performance/common.py
@@ -5,6 +5,7 @@ from typing import Dict, List
 from lisa import notifier
 from lisa.environment import Environment
 from lisa.notifier import DiskPerformanceMessage, DiskSetupType, DiskType
+from lisa.util import dict_to_fields
 
 
 def handle_and_send_back_results(
@@ -18,25 +19,12 @@ def handle_and_send_back_results(
     block_size: int = 4,
 ) -> None:
     information: Dict[str, str] = environment.get_information()
-    host_type = information.pop("platform", "")
-    host_by = information.pop("location", "")
-    host_os = information.pop("host_version", "")
-    guest_distro = information.pop("distro_version", "")
-    guest_size = information.pop("vmsize", "")
-    kernel_version = information.pop("kernel_version", "")
-    lis_version = information.pop("lis_version", "")
     for fio_message in fio_messages:
+        fio_message = dict_to_fields(information, fio_message)
         fio_message.core_count = core_count
         fio_message.disk_count = disk_count
         fio_message.test_case_name = test_case_name
         fio_message.block_size = block_size
         fio_message.disk_setup_type = disk_setup_type
         fio_message.disk_type = disk_type
-        fio_message.guest_distro = guest_distro
-        fio_message.guest_size = guest_size
-        fio_message.host_by = host_by
-        fio_message.host_os = host_os
-        fio_message.host_type = host_type
-        fio_message.kernel_version = kernel_version
-        fio_message.lis_version = lis_version
         notifier.notify(fio_message)
